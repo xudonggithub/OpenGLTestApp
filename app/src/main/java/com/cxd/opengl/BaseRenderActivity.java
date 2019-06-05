1ed74be76fb5385fc8a6622ca2bf1f3b9c65d519
xdchen <xudonggithub>
2019-06-05 18:09:58 +0800

    add

--- app/src/main/java/com/cxd/opengl/BaseRenderActivity.java
+++ app/src/main/java/com/cxd/opengl/BaseRenderActivity.java
@@ -5,34 +5,67 @@ import android.graphics.PixelFormat;
 import android.opengl.GLES20;
 import android.opengl.GLES30;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.cxd.utils.BitmapUtil;
 import com.cxd.utils.TextResourceReader;
 
-import com.arcsoft.utils.BitmapUtil;
 
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class BaseRenderActivity extends AppCompatActivity implements GLSurfaceView.Renderer{
+    protected String TAG = "CXDRender";
     protected GLSurfaceView mGLSurfaceView;
     protected int vertexShaderID = 0, fragmentShaderID = 0, program = 0, textureID = 0;
-    final float[] position = {
-            -1.0f,1.0f,0.0f,
-            1.0f,1.0f,0.0f,
-            -1.0f, -1.0f,0.0f,
-            1.0f, -1.0f,0.0f  };
 
-    final float[] texture = {
-           0.0f,0.0f,
+    final float[] position = {
+            //front
+            -0.5f,0.5f,0.5f,
+            0.5f,0.5f,0.5f,
+            -0.5f, -0.5f,0.5f,
+            0.5f, -0.5f, 0.5f,
+
+            //back
+            -0.5f,0.5f,-0.5f,
+            0.5f,0.5f,-0.5f,
+            -0.5f, -0.5f,-0.5f,
+            0.5f, -0.5f, -0.5f,
+
+            //right
+            0.5f,-0.5f, 0.5f,
+            0.5f,-0.5f, -0.5f,
+            0.5f, 0.5f, 0.5f,
+            0.5f, 0.5f, -0.5f,
+    };
+
+    final float[] texture = {//个数与上面的vertex一致，不然后面的会黑
+            0.0f,0.0f,
+            1.0f, 0.0f,
             0.0f,1.0f,
             1.0f, 1.0f,
-            1.0f, 0.0f  };
 
+            0.0f,0.0f,
+            1.0f, 0.0f,
+            0.0f,1.0f,
+            1.0f, 1.0f,
+
+            0.0f,0.0f,
+            1.0f, 0.0f,
+            0.0f,1.0f,
+            1.0f, 1.0f,
+             };
+
+    final FloatBuffer posFloatBuf = getGLBuffer(position);//java和opengl有大小头区别，记得native order
+    final FloatBuffer textFloatBuf = getGLBuffer(texture);
+    private float[] modelMatrix = new float[16];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,26 +78,43 @@ public class BaseRenderActivity extends AppCompatActivity implements GLSurfaceVi
         mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
         mGLSurfaceView.getHolder().setFormat(android.graphics.PixelFormat.TRANSLUCENT);
 
+        Matrix.setIdentityM(modelMatrix, 0);
+
+        Matrix.rotateM(modelMatrix,0, -45,0,1,0);//顺序
+        Matrix.rotateM(modelMatrix,0, 30,1,0,0);
     }
 
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
 
         vertexShaderID = GLES30.glCreateShader(GLES30.GL_VERTEX_SHADER);
-        GLES30.glShaderSource(vertexShaderID, TextResourceReader.readTextFileFromResource(this, R.raw.base_vertex_shader));
+        String vertexShader = TextResourceReader.readTextFileFromResource(this, R.raw.base_vertex_shader);
+        Log.d(TAG, vertexShader);
+        GLES30.glShaderSource(vertexShaderID, vertexShader);
         GLES30.glCompileShader(vertexShaderID);
-
         fragmentShaderID = GLES30.glCreateShader(GLES30.GL_FRAGMENT_SHADER);
-        GLES30.glShaderSource(fragmentShaderID, TextResourceReader.readTextFileFromResource(this, R.raw.base_fragment_shader));
-        GLES30.glCompileShader(fragmentShaderID);
 
+        String fragmentShader = TextResourceReader.readTextFileFromResource(this, R.raw.base_fragment_shader);
+        Log.d(TAG, fragmentShader);
+        GLES30.glShaderSource(fragmentShaderID, fragmentShader);
+        GLES30.glCompileShader(fragmentShaderID);
         program = GLES30.glCreateProgram();
         GLES30.glAttachShader(program, vertexShaderID);
         GLES30.glAttachShader(program, fragmentShaderID);
         GLES30.glLinkProgram(program);
+        int[] linkStatus = new int[1];
+        GLES30.glGetProgramiv(program, GLES30.GL_LINK_STATUS, linkStatus, 0);
+        if (linkStatus[0] != GLES20.GL_TRUE) {
+            Log.e(TAG, "Could not link program: ");
+            Log.e(TAG, GLES20.glGetProgramInfoLog(program));
+            GLES20.glDeleteProgram(program);
+            program = 0;
+        }
 
         Bitmap bitmap = BitmapUtil.decodeResource(this, R.raw.tex_0);
-        textureID = createImageTexture(ByteBuffer.wrap(BitmapUtil.bitmapToBytes(bitmap)), bitmap.getWidth(), bitmap.getHeight());
+        ByteBuffer byteBuffer = BitmapUtil.getByteBuffer(bitmap);
+        byteBuffer.position(0);
+        textureID = createImageTexture(byteBuffer, bitmap.getWidth(), bitmap.getHeight());
         bitmap.recycle();
 
     }
@@ -76,26 +126,39 @@ public class BaseRenderActivity extends AppCompatActivity implements GLSurfaceVi
 
     @Override
     public void onDrawFrame(GL10 gl10) {
-        GLES30.glClear(GLES20.GL_COLOR_BUFFER_BIT|GLES20.GL_DEPTH_BUFFER_BIT);
+        GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT|GLES30.GL_DEPTH_BUFFER_BIT);
+        GLES30.glClearColor(0.4f,0.4f,0.4f,1.0f);
+        GLES30.glViewport(0,0,1024,1024);
 
         GLES30.glUseProgram(program);
 
         GLES30.glEnableVertexAttribArray(0);
-        GLES30.glVertexAttribPointer(0, 3, GLES20.GL_FLOAT, false,  0, FloatBuffer.wrap(position));
+        posFloatBuf.position(0);//容易踩的坑，小心
+        GLES30.glVertexAttribPointer(0, 3, GLES30.GL_FLOAT, false, 0, posFloatBuf);
 
         GLES30.glEnableVertexAttribArray(1);
-        GLES30.glVertexAttribPointer(0, 2, GLES20.GL_FLOAT, false,  0, FloatBuffer.wrap(texture));
+        textFloatBuf.position(0);
+        GLES30.glVertexAttribPointer(1, 2, GLES30.GL_FLOAT, false, 0, textFloatBuf);
 
-        GLES30.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLES30.glActiveTexture(GLES30.GL_TEXTURE0);
         GLES30.glUniform1i(GLES30.glGetUniformLocation(program, "uTextue"), 0);
-        GLES30.glBindTexture(GLES20.GL_TEXTURE_2D, textureID);
+        GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, textureID);
+
+        GLES30.glUniformMatrix4fv(GLES30.glGetUniformLocation(program, "modelMatrix"), 1, false, modelMatrix, 0);
+
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLE_STRIP,0,4);
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLE_STRIP,4,4);
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLE_STRIP,8,4);
 
         GLES30.glDisableVertexAttribArray(0);
         GLES30.glDisableVertexAttribArray(1);
-        GLES30.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+        GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, 0);
         GLES30.glUseProgram(0);
     }
 
+private void checkError(String msg){
+    Log.d("cxd", msg+":"+GLES30.glGetError());
+}
     @Override
     protected void onResume() {
         mGLSurfaceView.onResume();
@@ -148,4 +211,12 @@ public class BaseRenderActivity extends AppCompatActivity implements GLSurfaceVi
         return textureID;
     }
 
+    private static  FloatBuffer getGLBuffer(float[] srcByte){
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4*srcByte.length);
+        byteBuffer.order(ByteOrder.nativeOrder());
+        FloatBuffer floatBuffer = byteBuffer.asFloatBuffer();
+        floatBuffer.put(srcByte);
+        byteBuffer.position(0);
+        return floatBuffer;
+    }
 }
--- app/src/main/java/com/cxd/opengl/MainActivity.java
+++ app/src/main/java/com/cxd/opengl/MainActivity.java
@@ -1,38 +1,98 @@
 package com.cxd.opengl;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
 
+    private static final  int INIT_PERMISSION=1000;
+    private String[] permssions={
+            Manifest.permission.CAMERA,
+            Manifest.permission.WAKE_LOCK,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+    };
+    private Handler  handler = new Handler();
+
+    private void initPermission(){
+        ArrayList<String> currentPermission=new ArrayList<String>();
+        for(int i=0;i<permssions.length;i++){
+            if(ContextCompat.checkSelfPermission(this, permssions[i])!= PackageManager.PERMISSION_GRANTED){
+                currentPermission.add(permssions[i]);
+            }
+        }
+
+        String[] usedPermissions=new String[currentPermission.size()];
+        currentPermission.toArray(usedPermissions);
+        if(usedPermissions.length>0) {
+            ActivityCompat.requestPermissions(this, usedPermissions, INIT_PERMISSION);
+        } else{
+            Intent intent=new Intent(this,BaseRenderActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case INIT_PERMISSION: {
+                for(int i=0;i<grantResults.length;i++){
+                    if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "必要权限未开启", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                }
+                Intent intent=new Intent(this,BaseRenderActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.base_texture);
-        tv.setText(stringFromJNI());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    initPermission();
+                }
+            }
+        },300);
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.base_texture:
-
+                Intent intent=new Intent(this,BaseRenderActivity.class);
+                startActivity(intent);
+                finish();
                 break;
         }
     }
