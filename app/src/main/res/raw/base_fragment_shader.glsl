8945c8f94fb22b2bf6320a8486b35105f652b6b0
xdchen <xudonggithub>
2019-06-08 17:56:26 +0800

    add

---
+++ app/src/main/res/raw/base_fragment_shader.glsl
@@ -0,0 +1,14 @@
+//#version 120
+#version 300 es
+precision highp float;
+
+//uniform vec2 test;
+uniform sampler2D uTextue;
+in vec2 v_Text;
+in vec4 v_Color;
+
+out vec4 fragColor;
+void main() {
+    fragColor = v_Color;//vec4(1.0,0,0,1.0);//texture(uTextue, v_Text);//
+//    gl_PointSize = 10.0;
+}
---
+++ app/src/main/res/raw/base_vertex_shader.glsl
@@ -0,0 +1,19 @@
+//#version 120
+#version 300 es
+
+uniform mat4 projectMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 modelMatrix;
+
+
+layout(location = 0) in vec4 aPos;
+layout(location = 1) in vec2 aText;
+layout(location = 2) in vec4 aColor;
+out vec2 v_Text;
+out vec4 v_Color;
+
+void main() {
+    gl_Position = projectMatrix*viewMatrix*modelMatrix*aPos;//projectMatrix*viewMatrix*modelMatrix*aPos;
+    v_Text = aText;
+    v_Color = aColor;
+}
